a0c00bdb3141bfe483b7db476217b973f5ddc3f1
ychesniuk <yurii.chesniuk@titanbay.com>
2023-03-22 18:52:32 +0200

    changed base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://twisteriokovel.github.io/',
+  base: '/cv',
   integrations: [tailwind()]
 });
